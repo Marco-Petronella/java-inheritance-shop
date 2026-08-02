0dbb99498d3cadd5a20b0855aeea0527917f06d8
Marco-Petronella <marco.petronella85@gmail.com>
2026-08-02 12:43:45 +0200

    chart correctly populated on Main

--- src/org/exercise/java/shop/Main.java
+++ src/org/exercise/java/shop/Main.java
@@ -22,10 +22,16 @@ public class Main {
         // testCarrello.addAcquisto(testCuffie);
         // System.out.println(testCarrello);
 
+        String nome;
+        String marca;
+        BigDecimal prezzo;
+        int iva;
         String readingLine;
         int readingInt;
+        Carrello mioCarrello = new Carrello();
         Scanner scan = new Scanner(System.in);
         boolean isShoppingDone = false;
+
         while (!isShoppingDone) {
             System.out.println("hai articoli da inserire? S/N");
             readingLine = scan.nextLine();
@@ -34,20 +40,70 @@ public class Main {
             } else if (readingLine.toUpperCase().equals("S")) {
                 System.out.println("Scegliere articolo da inserire \n 1) Televisore \n 2) Smartphone \n 3) Cuffie");
                 readingInt = Integer.parseInt(scan.nextLine());
-                if (readingInt == 1) {
-                    System.out.println("aggiungio TV");
-                } else if (readingInt == 2) {
-                    System.out.println("aggiungo Smartphone");
-                } else if (readingInt == 3) {
-                    System.out.println("aggiungo cuffie");
+                if (readingInt != 1 && readingInt != 2 && readingInt != 3) {
+                    System.out.println("input per tipo di oggetto non valido");
                 } else {
-                    System.out.println("input non valido");
+                    // inserisco i parametri comuni per tutti i Prodotti
+                    System.out.println("inserisci nome dell'oggetto: ");
+                    nome = scan.nextLine();
+                    System.out.println("inserisci marca dell'oggetto: ");
+                    marca = scan.nextLine();
+                    System.out.println("inserisci prezzo senza iva dell'oggetto: ");
+                    prezzo = new BigDecimal(scan.nextLine());
+                    System.out.println("inserisci IVA (default 22): ");
+                    iva = Integer.parseInt(scan.nextLine());
+
+                    if (readingInt == 1) {
+                        // inserisco i parametri specifici per le tv
+                        System.out.println("Inserisci dimensione della tv: ");
+                        String dimensione = scan.nextLine();
+                        System.out.println("E' una smart tv (S/N)");
+                        readingLine = scan.nextLine();
+                        boolean isSmart = false;
+                        if (readingLine.toUpperCase().equals("N")) {
+                            isSmart = false;
+                        } else if (readingLine.toUpperCase().equals("S")) {
+                            isSmart = true;
+                        }
+
+                        // aggiungo la tv al carrello
+                        Televisore newTelevisore = new Televisore(nome, marca, prezzo, iva, dimensione, isSmart);
+                        mioCarrello.addAcquisto(newTelevisore);
+                    } else if (readingInt == 2) {
+                        // inserisco i parametri specifici per gli smartphone
+                        System.out.println("Inserisci memoria dello smartphone: ");
+                        String memory = scan.nextLine();
+                        System.out.println("inserisci codice IMEI: ");
+                        String imei = scan.nextLine();
+
+                        // aggiungo lo smartphone al carrello
+                        Smarthphone newSmarthphone = new Smarthphone(nome, marca, prezzo, iva, imei, memory);
+                        mioCarrello.addAcquisto(newSmarthphone);
+
+                    } else if (readingInt == 3) {
+                        // inserisco i parametri specifici per le cuffie
+                        System.out.println("Inserisci colore delle cuffie: ");
+                        String colore = scan.nextLine();
+                        System.out.println("Sono wireless (S/N)");
+                        readingLine = scan.nextLine();
+                        boolean isWireless = false;
+                        if (readingLine.toUpperCase().equals("N")) {
+                            isWireless = false;
+                        } else if (readingLine.toUpperCase().equals("S")) {
+                            isWireless = true;
+                        }
+
+                        // aggiungo le cuffie
+                        Cuffie newCuffie = new Cuffie(nome, marca, prezzo, iva, colore, isWireless);
+                        mioCarrello.addAcquisto(newCuffie);
+                    }
                 }
             } else {
                 System.out.println("Input non valido! Inserisci 'S' per aggiungere articoli o 'N' per terminare");
             }
         }
 
+        System.out.println(mioCarrello);
         scan.close();
     }
 }
